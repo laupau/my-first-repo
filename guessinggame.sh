66016f63f9c0d0f805071ccef793b46aa7d880a4
Laura Paulina <60011647+laupau@users.noreply.github.com>
2021-02-15 23:50:20 -0600

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,21 @@
-hola
+##start
+int files=5
+echo "Welcome to the game!"
+echo "How many files are in the current directory?"
+receive num
+for (int i=0; i<5;i++) {
+function guess {
+	if [[guess==5]] {
+
+		echo "That is correct!" }
+
+	else if [[ guess < files ]] {
+
+			echo "Incorrect, the number is too low, try again" }
+  
+  	else if [[ guess > files ]] {
+    
+      echo "Incorrect, the number is too high, try again" }
+}
+}
+
