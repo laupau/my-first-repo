eed3a4bfa180584b7366fdfbcf43766d57750df5
Laura Paulina <60011647+laupau@users.noreply.github.com>
2021-02-18 09:27:29 -0600

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,5 @@
-README.md
-##start
+#!/usr/bin/env bash
+
 int files=5
 echo "Welcome to the game!"
 echo "How many files are in the current directory?"
@@ -7,19 +7,20 @@ receive num
 do {
 for (int i=0; i<5;i++) {
 function guess {
-	if [[guess==5]] {
+	if [[$guess==5]] {
 
 		echo "That is correct!" }
 
-	else if [[ guess < files ]] {
+	else if [[ $guess < $files ]] {
 
 			echo "Incorrect, the number is too low, try again" }
   
-  	else if [[ guess > files ]] {
+  	else if [[ $guess > $files ]] {
     
       echo "Incorrect, the number is too high, try again" } 
 }
 }
 }
 while num=!5;
+guessinggame
 
